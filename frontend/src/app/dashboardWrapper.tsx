9a2c9366ed0cadfdb38b47dea4f0adcd45b4eae3
Vynitial <109939562+yorunaka@users.noreply.github.com>
2025-08-12 13:58:58 +0700

    added collapsing sidebar logic

--- frontend/src/app/dashboardWrapper.tsx
+++ frontend/src/app/dashboardWrapper.tsx
@@ -1,7 +1,8 @@
+"use client"
 import React from 'react'
 import Navbar from './(components)/Navbar/Navbar'
 import Sidebar from './(components)/Sidebar/Sidebar'
-import StoreProvider from './redux'
+import StoreProvider, { useAppSelector } from './redux'
 
 const DashboardWrapper = ({children} : {children : React.ReactNode}) => {
   return (
@@ -13,10 +14,14 @@ const DashboardWrapper = ({children} : {children : React.ReactNode}) => {
   )}
 
 const DashboardLayout = ({children} : {children : React.ReactNode}) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  )
+  
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
         <Sidebar />
-        <main className='flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24'>
+        <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? 'md:pl-24' : 'md:pl-72'}`}>
             <Navbar />
             {children}
         </main>
